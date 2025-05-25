c0a9909413a06b49ab6672d728867adc5849a225
morgana-x <morgana.is.sugoi@gmail.com>
2025-05-25 20:06:26 +1000

    Move creation of listings_basic table to settings.php

--- project2/settings.php
+++ project2/settings.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "EOI"; // Lachlan
+$database = "EOI";
 
 $conn = mysqli_connect($host, $username, $password, $database);
 
@@ -10,6 +10,7 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+// Lachlan - EOI table
 $conn->query("CREATE TABLE IF NOT EXISTS EOI (
     eoi_number INT AUTO_INCREMENT PRIMARY KEY,
     eoi_status VARCHAR(7) NOT NULL,
@@ -28,9 +29,23 @@ $conn->query("CREATE TABLE IF NOT EXISTS EOI (
     skills_other VARCHAR(2048) NOT NULL
     )");
 
+// Lachlan - ACCOUNTS table
 $conn->query("CREATE TABLE IF NOT EXISTS ACCOUNTS (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     user_name VARCHAR(50) NOT NULL,
     user_password VARCHAR(50) NOT NULL
     )");
+
+// Marcus - listings_basic table
+$conn->query("CREATE TABLE IF NOT EXISTS `listings_basic` (
+  `ref_id` varchar(5) NOT NULL,
+  `position` varchar(30) NOT NULL,
+  `salary` varchar(20) NOT NULL,
+  `yoe` varchar(20) NOT NULL,
+  `desc` text NOT NULL,
+  `responsibilities` text NOT NULL,
+  `essen_qual` text NOT NULL,
+  `pref_qual` text NOT NULL
+   )");
+
 ?>
