d4ee643c7c9f8252776e074a10ab277a44aa2f9f
morgana-x <morgana.is.sugoi@gmail.com>
2025-05-25 21:10:07 +1000

    Add some comments

--- project2/process_register.php
+++ project2/process_register.php
@@ -20,42 +20,41 @@
         exit();
     }
 
-    if (!isset($_POST["reg_username"]) )
+    if (!isset($_POST["reg_username"]) ) // check user provided a username
         register_error_page();
 
-    if (!isset($_POST["reg_password"]) )
+    if (!isset($_POST["reg_password"]) )// check user provided a password
         register_error_page();
 
     include("settings.php");
 
-    $name = mysqli_escape_string($conn, $_POST["reg_username"]);
+    $name = mysqli_escape_string($conn, $_POST["reg_username"]); // ensure values are SQL query safe
     $pw   = mysqli_escape_string($conn, $_POST["reg_password"]);
+
     unset($_POST["reg_username"]);
     unset($_POST["reg_password"]);
 
     $result = $conn->query("SELECT * FROM ACCOUNTS WHERE user_name = '$name'");
 
-    if (mysqli_num_rows($result) > 0)
+    if (mysqli_num_rows($result) > 0) // Abort if there are any accounts with the same username
         register_error_page("An account called $name already exists!");
-
         
     $result = $conn->query("INSERT INTO ACCOUNTS (user_name, user_password) VALUES ('$name', '$pw')");
 
-    if (!$result)
+    if (!$result) // Catch SQL error
         register_error_page( mysqli_error($conn));
-    //header("Location: manage.php");
 
-      include("header.inc");
-        include("nav.inc");
-        echo('<body>
-        <div class="site_content site_form_background"> <!--Lachlan - Site content class is like a custom body class for a div in this case-->
-            <br>  
-        <div class = "site_form_container">
-                <h1>Registration Success!</h1> <!--Title-->    
-                <p>Your account was successfully created!</p>
-                <h2><a href="login.php">Login to your account here!</a></h2>
-            </div>
-        </div>');
-        include("footer.inc");
-        exit();
+    include("header.inc");
+    include("nav.inc");
+    echo('<body>
+    <div class="site_content site_form_background"> <!--Lachlan - Site content class is like a custom body class for a div in this case-->
+        <br>  
+    <div class = "site_form_container">
+            <h1>Registration Success!</h1> <!--Title-->    
+            <p>Your account was successfully created!</p>
+            <h2><a href="login.php">Login to your account here!</a></h2>
+        </div>
+    </div>');
+    include("footer.inc");
+    exit();
 ?>
